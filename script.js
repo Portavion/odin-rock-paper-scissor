24f889c672fbcea1c2592ba9e18c4358fb77859f
Portavion <156545430+Portavion@users.noreply.github.com>
2024-08-09 19:22:33 +0200

    Add function getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Yp')
+function getComputerChoice() {
+    // Returns the computer choice amongst rock paper or scissor
+    let randomChoice = Math.floor(Math.random()*3);
+    let computerChoice = '';
+
+    if (randomChoice === 0) {
+        computerChoice = 'rock';
+    }
+    else if (randomChoice === 1) {
+        computerChoice = 'paper';
+    }
+    else {
+        computerChoice = 'scissors'
+    }
+
+
+    return computerChoice;
+}
